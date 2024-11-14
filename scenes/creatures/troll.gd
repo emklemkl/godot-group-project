ee2c22dc58c839ba9bbed821669aa59dce7420fd
Emil Karlsson <emil94_karlsson@outlook.com>
2024-11-14 20:41:32 +0100

    bugfix, troll is now consistently following player

--- scenes/creatures/troll.gd
+++ scenes/creatures/troll.gd
@@ -1,12 +1,11 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 3.0
 const JUMP_VELOCITY = 4.5
 
 
 func get_pos() -> Vector3:
-
 	var player = get_parent().get_node("Player")
 	if player:
 		return player.global_transform.origin
@@ -14,8 +13,9 @@ func get_pos() -> Vector3:
 	return pos
 
 func _physics_process(delta: float) -> void:
-
 	var player_cur_pos = get_pos()
-	velocity.x = move_toward(velocity.x, player_cur_pos.x, SPEED)
-	velocity.z = move_toward(velocity.z, player_cur_pos.z, SPEED)
+	var troll_cur_pos = self.global_transform.origin
+	var direction = (player_cur_pos - troll_cur_pos).normalized()
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
 	move_and_slide()
