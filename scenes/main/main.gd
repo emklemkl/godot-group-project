cc16083d58a4f9b3e1ce58f8b509a14da625b238
Erik <erlo22@student.bth.se>
2024-11-14 18:57:07 +0100

    bäsak

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -4,6 +4,8 @@ extends Node3D
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,8 +2,8 @@ extends CharacterBody3D
 
 var hp = 100
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var speed = 7.5
-var jump_speed = 10
+var speed = 5
+var jump_speed = 3.5
 var mouse_sensitivity = 0.002
 
 func ready():
@@ -14,6 +14,8 @@ func _input(event):
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
 		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+	if Input.is_action_just_pressed("interact"):
+		interact()
 
 func _physics_process(delta):
 	velocity.y += -gravity * delta
@@ -25,3 +27,10 @@ func _physics_process(delta):
 	move_and_slide()
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = jump_speed
+
+func interact():
+	pass
+
+
+func _on_area_3_darm_body_entered(body: Node3D) -> void:
+	print(body)
