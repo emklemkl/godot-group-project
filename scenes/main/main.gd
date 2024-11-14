e116d0125225728e65f682b2abf2581f8463a9bc
Emil Karlsson <emil94_karlsson@outlook.com>
2024-11-14 19:33:52 +0100

    added troll to main scene

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -4,6 +4,7 @@ extends Node3D
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
