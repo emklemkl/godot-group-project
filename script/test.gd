07cd4bff3a615ee468d0d71d7abed56c8a721e12
Erik <erlo22@student.bth.se>
2024-11-14 14:33:08 +0100

    testar att lägga upp lite saker

--- script/test.gd
+++ script/test.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body
